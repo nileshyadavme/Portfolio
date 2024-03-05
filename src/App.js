ef21513e77b485298dc019bde428cb0bbe2be986
nilesh <contact.nileshy@gmail.com>
2024-03-05 20:28:29 +0530

    added data & Social

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import Home from './components/Home/Home';
 import Header from './components/header/Header';
 function App() {
   return (
-    <Header />
+    <Home />
   );
 }
 
---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const Social = () =>{
+    return(
+        <div className="home__social">
+            {/* instagram */}
+            <a href="https://instagram.com/nileshyadavme" className="home__social-icon" target="_blank"><i class="uil uil-instagram"></i></a>
+            {/* github */}
+            <a href="https://github.com/nileshyadavme" className="home__social-icon" target="_blank"><i class="uil uil-github"></i></a>
+            {/* linkedin */}
+            <a href="https://linkedin.com/in/nileshyadavme/" className="home__social-icon" target="_blank"><i class="uil uil-linkedin"></i></a>
+        </div>
+    )
+}
+export default Social;
