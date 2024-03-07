3faf93dd6aafdb9e16ec53e84cceff5331c5031e
nilesh <contact.nileshy@gmail.com>
2024-03-07 21:34:12 +0530

    imported About.jsx in App.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './components/Home/Home';
 import Header from './components/header/Header';
+import About from './components/about/About';
 function App() {
   return (
     <>
     <Header />
     <main className='main'>
       <Home />
+      <About />
     </main>
     </>
   );
---
+++ src/components/about/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export class About extends PureComponent {
+  render() {
+    return (
+      <div>About</div>
+    )
+  }
+}
+
+export default About
