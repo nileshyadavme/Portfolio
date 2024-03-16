de206c3ecb769163da9d11d120f09c4910ccf99e
nilesh <contact.nileshy@gmail.com>
2024-03-16 19:24:21 +0530

    Info.jsx updated

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -3,7 +3,18 @@ import React, { PureComponent } from 'react'
 export class Info extends PureComponent {
   render() {
     return (
-      <div>info</div>
+        <div className="about__info grid">
+            <div className="about__box">
+            <i class='bx bx-award' ></i>
+                <h3 className='about__title'>Experience</h3>
+                <span className='about__subtitle'>1 Years Working</span>
+            </div>
+            <div className="about__box">
+            <i class='bx bx-briefcase-alt' ></i>
+                <h3 className='about__title'>Completed</h3>
+                <span className='about__subtitle'>5 Projects</span>
+            </div> 
+        </div>
     )
   }
 }
